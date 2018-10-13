8894cd45ec95b9f595e6d53710d16846652b680b
feim <igmo199@gmail.com>
2018-10-14 00:51:05 +0200

    split and count number of items

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 (
   function () {
-
+    // Defining App & controller lunchCheckerApp & lunchCheckerController
     angular.module('lunchCheckerApp',[]).controller('lunchCheckerController',lunchCheckerController);
+    //  inject scope & filter
     lunchCheckerController.$inject = ["$scope","$filter"]
+
     function lunchCheckerController($scope , $filter) {
          $scope.check = function () {
+           // Recover data input
+             var dishes = ($scope.dishes).split(',')
+             console.log(dishes);
 
          }
       }
