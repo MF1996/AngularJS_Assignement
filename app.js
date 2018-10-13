244c05034d234f8e75b9ec1f1ee45fbf39e9cb81
feim <igmo199@gmail.com>
2018-10-14 01:18:14 +0200

    adding condition that input data are not space

--- app.js
+++ app.js
@@ -9,12 +9,17 @@
          $scope.check = function () {
 
            // Recover data input
-           if($scope.dishes != undefined){
+           if($scope.dishes != undefined && $scope.dishes != ""){
              var number = ($scope.dishes).split(',').length
-             if(number <= 3){
-               $scope.respond ="Enjoy !!"
-             }else{
-               $scope.respond ="Too much!"
+             console.log(number)
+             if(number == 0 ){
+                $scope.respond = " Please enter data first "
+             }else {
+               if(number <= 3){
+                 $scope.respond ="Enjoy !!"
+               }else{
+                 $scope.respond ="Too much!"
+               }
              }
            }else{
              $scope.respond = " Please enter data first "
