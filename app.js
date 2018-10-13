9d8d37db9cc16283a2332cc76ce2b343aa2b97e8
feim <igmo199@gmail.com>
2018-10-14 00:42:49 +0200

    Define check method

--- app.js
+++ app.js
@@ -4,11 +4,8 @@
     angular.module('lunchCheckerApp',[]).controller('lunchCheckerController',lunchCheckerController);
     lunchCheckerController.$inject = ["$scope","$filter"]
     function lunchCheckerController($scope , $filter) {
-         $scope.check = function (){
-           var split = $filter('split')
-           var dishes = split(",",$scope.dishes)
-           $scope.respond = dishes
-           console.log(dishes)
+         $scope.check = function () {
+           
          }
       }
     }
